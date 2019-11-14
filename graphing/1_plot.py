1c92672962beb161ae92aef7ba89923bfb83b531
SabrinaJimm <54287277+SabrinaJimm@users.noreply.github.com>
2019-11-14 14:47:43 -0600

    Update 1_plot.py

--- graphing/1_plot.py
+++ graphing/1_plot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 # %matplotlib_inline #para jupyter notebook
 #Grafica 2 vectores
-plt.plot([1,2,3,4]) #Vector en Y
+var.plot(["Algoritmos", "contabilidad", "T.I", "T.C", "Info 1)"],[9 , 6,10,10,5]'b-*') #Vector en Y
 #Grafica por defecto el vecto X = 1, 2, 3, 4
 plt.title('Title')
-plt.ylabel('some Y numbers')
+plt.ylabel('contabilidad, operaciones,')
 plt.xlabel('some X numbers')
 plt.show()
